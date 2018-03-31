088258643a1668b2cccd1855efd158d8edc67554
brandon.giraldo <brandon.giraldo@workday.com>
2018-03-31 14:41:45 -0700

    TypeScript + React + Webpack integration, will remove angular
    Change-Id: I7d46959c505ccaf5878862eee602f510ca258edc

---
+++ app.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+import Board from './src/components/board/board'
+
+ReactDOM.render(
+<div>
+  <h1>Hello, Welcome to the first page</h1>
+  <Board/>
+</div>,
+  document.getElementById("root")
+);
